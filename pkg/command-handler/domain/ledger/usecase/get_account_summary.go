0040ba4c49acbf634692b0dd67bf446bd4baae72
Alexandre Liberato <alexandre@feiler.co>
2021-01-17 20:17:55 -0300

    feature/34 - Synthetic Account Reports - created repository and usecase.

---
+++ pkg/command-handler/domain/ledger/usecase/get_account_summary.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"context"
+
+	"time"
+
+	"github.com/jackc/pgx/v4"
+
+	"github.com/stone-co/the-amazing-ledger/pkg/command-handler/domain/ledger/entities"
+)
+
+func (l *LedgerUseCase) RetrieveSummaryReport(ctx context.Context, accountName entities.AccountName, startTime time.Time, endTime time.Time) (*entities.AccountBalance, error) {
+
+	accountBalance, err := l.repository.GetAccountSummary(ctx, accountName, startTime, endTime)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, entities.ErrAccountNotFound
+		}
+		return nil, err
+	}
+
+	return accountBalance, nil
+}
